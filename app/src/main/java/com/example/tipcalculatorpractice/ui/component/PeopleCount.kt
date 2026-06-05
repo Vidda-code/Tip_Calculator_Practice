62bc5254723ff44e6913b4c63c0e25a5096aeecc
David Godfrey <kinqvidda@gmail.com>
2026-06-05 18:50:15 +0100

    Implementing PeopleCount component(Corrections made)

--- app/src/main/java/com/example/tipcalculatorpractice/ui/component/PeopleCount.kt
+++ app/src/main/java/com/example/tipcalculatorpractice/ui/component/PeopleCount.kt
@@ -1,35 +1,69 @@
 package com.example.tipcalculatorpractice.ui.component
 
-import androidx.compose.foundation.layout.Box
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun PeopleCount(peopleCount: Int, tipPerPerson: Int) {
-    Box {
-        Row {
-            Card(
-
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Person,
-                    contentDescription = "Profile Logo"
-                )
-            }
-
-            Column {
-                Text(text = "Person $peopleCount")
-                Text(text = "Equal Share")
-            }
-
-            Text(text = "\\u20A6 $tipPerPerson")
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                horizontal = 6.dp,
+                vertical = 6.dp
+            ),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Card(
+            colors = CardDefaults.cardColors(
+                Color(0xFFAFCCC5)
+            ),
+            shape = RoundedCornerShape(6.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Person,
+                contentDescription = "Profile Logo",
+                modifier = Modifier.padding(4.dp)
+            )
         }
+
+        Spacer(modifier = Modifier.width(12.dp))
+
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = "Person $peopleCount",
+                fontSize = 18.sp
+            )
+            Text(
+                text = "Equal Share",
+                fontSize = 12.sp
+            )
+        }
+
+        Text(
+            text = "₦$tipPerPerson",
+            fontSize = 18.sp
+        )
     }
 }
-
